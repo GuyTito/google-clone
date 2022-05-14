88dab2a5a5309d9e1790c198bee9e68ee7ab5702
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-14 20:13:12 +0000

    Create data.js

---
+++ src/composables/data.js
@@ -0,0 +1,267 @@
+const data = {
+  results: [
+    {
+      title: "Electric Cars, Solar & Clean Energy | Tesla United Kingdom",
+      link: "https://www.tesla.com/en_gb",
+      description: "",
+      additional_links: [
+        {
+          text: "Electric Cars, Solar & Clean Energy | Tesla United Kingdomhttps://www.tesla.com › en_gb",
+          href: "https://www.tesla.com/en_gb"
+        },
+        {
+          text: "Model 3",
+          href: "https://www.tesla.com/en_gb/model3/design"
+        },
+        {
+          text: "Used Inventory",
+          href: "https://www.tesla.com/en_gb/inventory/used/ms"
+        },
+        {
+          text: "Model S",
+          href: "https://www.tesla.com/en_gb/models"
+        },
+        {
+          text: "Model Y",
+          href: "https://www.tesla.com/en_gb/modely/design"
+        }
+      ],
+      cite: {
+        domain: "https://www.tesla.com › en_gb",
+        span: " › en_gb"
+      }
+    },
+    {
+      title: "Electric Cars, Solar & Clean Energy | Tesla United Kingdom",
+      link: "https://www.tesla.com/en_gb",
+      description: "Tesla is accelerating the world's transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and ...",
+      additional_links: [
+        {
+          text: "Electric Cars, Solar & Clean Energy | Tesla United Kingdomhttps://www.tesla.com › en_gb",
+          href: "https://www.tesla.com/en_gb"
+        }
+      ],
+      cite: {
+        domain: "https://www.tesla.com › en_gb",
+        span: " › en_gb"
+      }
+    },
+    {
+      title: "Tesla, Inc. - Wikipedia",
+      link: "https://en.wikipedia.org/wiki/Tesla,_Inc.",
+      description: "Tesla, Inc is an American automotive and clean energy company based in Austin, Texas. Tesla designs and manufactures electric vehicles (electric cars and ... Owner:  : Elon Musk\u200e (17%);\u200e Total assets:  US$62.1 billion (2021) Products:  Model S; \u200eModel 3\u200e; \u200eModel X\u200e; \u200eModel Y\u200e; ...\u200e Total equity:  US$30.2 billion (2021)",
+      additional_links: [
+        {
+          text: "Tesla, Inc. - Wikipediahttps://en.wikipedia.org › wiki › Tesla,_Inc",
+          href: "https://en.wikipedia.org/wiki/Tesla,_Inc."
+        },
+        {
+          text: "Elon Musk",
+          href: "http://en.wikipedia.org/wiki/Elon_Musk"
+        },
+        {
+          text: "Model S",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_S"
+        },
+        {
+          text: "Model 3",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_3"
+        },
+        {
+          text: "Model X",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_X"
+        },
+        {
+          text: "Model Y",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_Y"
+        },
+        {
+          text: "",
+          href: "https://en.wikipedia.org/wiki/Tesla,_Inc."
+        },
+        {
+          text: "Nikola Tesla - Wikipediahttps://en.wikipedia.org › wiki › Nikola_Tesla",
+          href: "https://en.wikipedia.org/wiki/Nikola_Tesla"
+        },
+        {
+          text: "New York City",
+          href: "http://en.wikipedia.org/wiki/New_York_City"
+        },
+        {
+          text: "Smiljan",
+          href: "http://en.wikipedia.org/wiki/Smiljan"
+        },
+        {
+          text: "Austrian Empire",
+          href: "http://en.wikipedia.org/wiki/Austrian_Empire"
+        },
+        {
+          text: "Graz University of Technology",
+          href: "http://en.wikipedia.org/wiki/Graz_University_of_Technology"
+        },
+        {
+          text: "Order of St. Sava",
+          href: "http://en.wikipedia.org/wiki/Order_of_St._Sava"
+        },
+        {
+          text: "Governm...",
+          href: "http://en.wikipedia.org/wiki/Government_of_Serbia"
+        },
+        {
+          text: "",
+          href: "https://en.wikipedia.org/wiki/Nikola_Tesla"
+        }
+      ],
+      cite: {
+        domain: "https://en.wikipedia.org › wiki › Tesla,_Inc",
+        span: " › wiki › Tesla,_Inc"
+      }
+    },
+    {
+      title: "Tesla, Inc. - Wikipedia",
+      link: "https://en.wikipedia.org/wiki/Tesla,_Inc.",
+      description: "Tesla, Inc. - Wikipediahttps://en.wikipedia.org › wiki › Tesla,_Inchttps://en.wikipedia.org › wiki › Tesla,_IncTesla, Inc is an American automotive and clean energy company based in Austin, Texas. Tesla designs and manufactures electric vehicles (electric cars and ...Owner: : Elon Musk\u200e (17%);\u200eTotal assets: US$62.1 billion (2021)Products: Model S; \u200eModel 3\u200e; \u200eModel X\u200e; \u200eModel Y\u200e; ...\u200eTotal equity: US$30.2 billion (2021)",
+      additional_links: [
+        {
+          text: "Tesla, Inc. - Wikipediahttps://en.wikipedia.org › wiki › Tesla,_Inc",
+          href: "https://en.wikipedia.org/wiki/Tesla,_Inc."
+        },
+        {
+          text: "Elon Musk",
+          href: "http://en.wikipedia.org/wiki/Elon_Musk"
+        },
+        {
+          text: "Model S",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_S"
+        },
+        {
+          text: "Model 3",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_3"
+        },
+        {
+          text: "Model X",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_X"
+        },
+        {
+          text: "Model Y",
+          href: "http://en.wikipedia.org/wiki/Tesla_Model_Y"
+        },
+        {
+          text: "",
+          href: "https://en.wikipedia.org/wiki/Tesla,_Inc."
+        }
+      ],
+      cite: {
+        domain: "https://en.wikipedia.org › wiki › Tesla,_Inc",
+        span: " › wiki › Tesla,_Inc"
+      }
+    },
+    {
+      title: "Nikola Tesla - Wikipedia",
+      link: "https://en.wikipedia.org/wiki/Nikola_Tesla",
+      description: "Nikola Tesla - Wikipediahttps://en.wikipedia.org › wiki › Nikola_Teslahttps://en.wikipedia.org › wiki › Nikola_TeslaNikola Tesla was a Serbian-American inventor, electrical engineer, mechanical engineer, and futurist best known for his contributions to the design of the ...Died: 7 January 1943 (aged 86); New York City\u200e, ...\u200eBorn: 10 July 1856; Smiljan\u200e, \u200eAustrian Empire\u200e ...\u200eEducation: Graz University of Technology ...\u200eAwards: > Order of St. Sava\u200e, II Class, \u200eGovernm...\u200e",
+      additional_links: [
+        {
+          text: "Nikola Tesla - Wikipediahttps://en.wikipedia.org › wiki › Nikola_Tesla",
+          href: "https://en.wikipedia.org/wiki/Nikola_Tesla"
+        },
+        {
+          text: "New York City",
+          href: "http://en.wikipedia.org/wiki/New_York_City"
+        },
+        {
+          text: "Smiljan",
+          href: "http://en.wikipedia.org/wiki/Smiljan"
+        },
+        {
+          text: "Austrian Empire",
+          href: "http://en.wikipedia.org/wiki/Austrian_Empire"
+        },
+        {
+          text: "Graz University of Technology",
+          href: "http://en.wikipedia.org/wiki/Graz_University_of_Technology"
+        },
+        {
+          text: "Order of St. Sava",
+          href: "http://en.wikipedia.org/wiki/Order_of_St._Sava"
+        },
+        {
+          text: "Governm...",
+          href: "http://en.wikipedia.org/wiki/Government_of_Serbia"
+        },
+        {
+          text: "",
+          href: "https://en.wikipedia.org/wiki/Nikola_Tesla"
+        }
+      ],
+      cite: {
+        domain: "https://en.wikipedia.org › wiki › Nikola_Tesla",
+        span: " › wiki › Nikola_Tesla"
+      }
+    },
+    {
+      title: "Tesla (@Tesla) · Twitter",
+      link: "https://twitter.com/Tesla?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+      description: "",
+      cite: {}
+    },
+    {
+      title: "Tesla Inc (TSLA) Stock Price & News - Google Finance",
+      link: "https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en",
+      description: "Tesla, Inc. is an American automotive and clean energy company based in Austin, Texas. Tesla designs and manufactures electric vehicles, battery energy ...",
+      additional_links: [
+        {
+          text: "Tesla Inc (TSLA) Stock Price & News - Google Financehttps://www.google.com › quote › TSLA:NASDAQ",
+          href: "https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en"
+        }
+      ],
+      cite: {
+        domain: "https://www.google.com › quote › TSLA:NASDAQ",
+        span: " › quote › TSLA:NASDAQ"
+      }
+    },
+    {
+      title: "Tesla News & Reviews | Auto Express",
+      link: "https://www.autoexpress.co.uk/tesla",
+      description: "Tesla CEO Elon Musk spoke about the Cybertruck at the company's shareholder meeting last night, stating they won't be launching any new vehicles in 20… 27 Jan ...",
+      additional_links: [
+        {
+          text: "Tesla News & Reviews | Auto Expresshttps://www.autoexpress.co.uk › tesla",
+          href: "https://www.autoexpress.co.uk/tesla"
+        }
+      ],
+      cite: {
+        domain: "https://www.autoexpress.co.uk › tesla",
+        span: " › tesla"
+      }
+    },
+    {
+      title: "TSLA: Tesla Inc - Stock Price, Quote and News - CNBC",
+      link: "https://www.cnbc.com/quotes/TSLA",
+      description: "Get Tesla Inc (TSLA:NASDAQ) real-time stock quotes, news, price and financial information from CNBC.",
+      additional_links: [
+        {
+          text: "TSLA: Tesla Inc - Stock Price, Quote and News - CNBChttps://www.cnbc.com › quotes › TSLA",
+          href: "https://www.cnbc.com/quotes/TSLA"
+        }
+      ],
+      cite: {
+        domain: "https://www.cnbc.com › quotes › TSLA",
+        span: " › quotes › TSLA"
+      }
+    }
+  ],
+  image_results: [],
+  total: 960000000,
+  answers: [
+    "How much are Teslas UK?",
+    "Which is the cheapest Tesla car?",
+    "How costly is a Tesla?",
+    "Is there Tesla in UK?"
+  ],
+  ts: 4.0030975341796875,
+  device_region: "EU",
+  device_type: "desktop"
+}
+
+export default data
