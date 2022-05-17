4bdec187192e45ce717cf28dbf51d475d6eb2d7e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-17 08:54:48 +0000

    style search results

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ const menu = ['All', 'Images', 'Videos', 'News']
 <template>
   <!-- <div class="min-h-screen  "> -->
     <div class=" mx-5 sm:mx-14 relative min-h-screen md:max-w-3xl md:mx-auto">
-      <form @submit.prevent="search" class="border-2 border-gray-300 rounded-lg p-4 flex items-center space-x-2 text-gray-400 w-full absolute transition-all duration-700 top-1/2 -translate-y-1/2" :class="{'top-5 translate-y-0': move_top}">
+      <form @submit.prevent="search" class="border-2 border-gray-300 rounded-lg p-4 flex items-center space-x-2 text-gray-400 w-full absolute transition-all duration-700 top-1/2 -translate-y-1/2 shadow-md" :class="{'top-5 translate-y-0': move_top}">
         <SearchIcon class="h-5 w-5" />
         <input type="text" v-model="search_term" @keydown="move_top = true" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black outline-none">
         <button type="submit" class=""></button>
@@ -48,7 +48,7 @@ const menu = ['All', 'Images', 'Videos', 'News']
       <div  class="opacity-0 transition-all transform scale-0 duration-300 delay-500 absolute top-20 mt-5 text-black w-full" :class="{'opacity-100 scale-100' : move_top}">
         <div v-if="move_top">
           <nav class="space-x-5 sm:space-x-10 flex sm:block justify-between mb-5 sm:mb-8 border-b-2 text-gray-400">
-            <a v-for="item in menu" :key="item" href="#" class="border-b-4 border-transparent py-2 px-4 inline-block hover:font-medium hover:text-black hover:border-black">
+            <a v-for="item in menu" :key="item" href="#" class="border-b-4 border-transparent py-2 px-4 inline-block hover:font-medium hover:text-green-600 hover:border-green-600">
               {{ item }}
             </a>
             <!-- apply hover properties when you install vue-router -->
@@ -56,9 +56,9 @@ const menu = ['All', 'Images', 'Videos', 'News']
 
           <div v-if="data" class="space-y-8">
             <div v-for="item in data.results" :key="item.id" class="">
-              <a :href="item.link" target="_blank" class="">
+              <a :href="item.link" target="_blank" rel="noopener" class="flex flex-col group">
                 <span>{{item.cite.domain}}</span>
-                <span>{{item.title}}</span>
+                <span class="font-semibold text-xl text-green-600 group-hover:underline">{{item.title}}</span>
               </a>
               <p> {{item.description}} </p>
             </div>
