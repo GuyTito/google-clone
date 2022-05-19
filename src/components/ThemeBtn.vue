9c62b0a0075a57e29f51606c9a72787d050ca55c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-19 12:07:52 +0000

    Create ThemeBtn.vue

---
+++ src/components/ThemeBtn.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+</script>
+
+
+<template>
+  <header>
+    <button @click="toggleDark()">
+      <span class="ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
+    </button>
+  </header>
+  
+</template>
+
+
+<style lang="postcss" scoped>
+
+</style>
