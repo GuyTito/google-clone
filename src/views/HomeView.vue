bcb3d32dd0ba4fddbabea8650d3587101b27c3e3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-19 10:46:55 +0000

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router'
+import Searchbar from '../components/Searchbar.vue';
+
+const router = useRouter()
+
+
+function search(search_term) {
+  if (search_term) {
+    router.push({ path: '/search', query: { q: search_term } })
+  }
+}
+
+</script>
+
+<template>
+  <div class="grid place-items-center min-h-screen">
+    <Searchbar @search="search"/>
+  </div>
+</template>
