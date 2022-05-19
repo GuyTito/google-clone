3837fb760f5675c0d177213998c3f4ef14fd6cee
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-19 10:47:13 +0000

    Create SearchView.vue

---
+++ src/views/SearchView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import Searchbar from '../components/Searchbar.vue';
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+
+const search_term = ref('')
+search_term.value = route.query.q || ''
+
+function search() {
+  
+}
+
+</script>
+
+
+<template>
+  <div class="flex justify-center">
+    <Searchbar :search_term="search_term" @search="search"/>
+  </div>
+  
+</template>
+
+
+<style lang="postcss" scoped>
+
+</style>
