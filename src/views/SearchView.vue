0fe06a3118bc4ab45c4f232d7d8725b9cc996ae0
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-19 12:06:41 +0000

    showed results, search function, active link

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,6 +2,9 @@
 import { ref } from 'vue';
 import Searchbar from '../components/Searchbar.vue';
 import { useRoute } from 'vue-router'
+import data from '@/composables/data.js'
+import { computed } from '@vue/reactivity';
+
 
 const route = useRoute()
 
@@ -9,21 +12,73 @@ const route = useRoute()
 const search_term = ref('')
 search_term.value = route.query.q || ''
 
-function search() {
-  
+// const data = ref(null)
+async function search() {
+  if (search_term.value) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-Proxy-Location': 'EU',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+        'X-RapidAPI-Key': import.meta.env.VITE_API_KEY
+      }
+    }
+
+    try {
+      const res = await fetch(`https://google-search3.p.rapidapi.com/api/v1/search/q=${search_term.value}`, options)
+      data.value = await res.json()
+    } catch (error) {
+      console.error(error.msg)
+    }
+  }
 }
+
+const menu = ref(['search', 'Images', 'Videos', 'News'])
 
 </script>
 
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center mt-5">
     <Searchbar :search_term="search_term" @search="search"/>
+  </div>
+
+  <div class=" mx-5 sm:mx-14 md:max-w-3xl md:mx-auto">
+    <!-- <div v-if="data" class="mt-5 text-black sm:max-w-xl">
+      {{ data }}
+    </div> -->
+    <div  class="mt-5 text-black w-full" >
+      <div>
+        <nav class="space-x-5 sm:space-x-10 flex sm:block justify-between mb-5 sm:mb-8 border-b-2 text-gray-400">
+          <router-link 
+            v-for="item in menu" :key="item" 
+            :to="`/${item.toLowerCase()}?q=${search_term}`" 
+            class="border-b-4 border-transparent py-2 px-4 inline-block "
+          >
+            {{ item == 'search' ? 'All' : item }}
+          </router-link>
+        </nav>
+
+        <div v-if="data" class="space-y-8">
+          <div v-for="item in data.results" :key="item.id" class="">
+            <a :href="item.link" target="_blank" rel="noopener" class="flex flex-col group">
+              <span>{{item.cite.domain}}</span>
+              <span class="font-semibold text-xl text-green-600 group-hover:underline">{{item.title}}</span>
+            </a>
+            <p> {{item.description}} </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
   </div>
   
 </template>
 
 
 <style lang="postcss" scoped>
-
+.router-link-exact-active{
+  @apply font-medium text-green-600 border-green-600
+}
 </style>
