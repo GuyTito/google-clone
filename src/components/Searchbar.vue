84cf394a5233d738ba8b1040ec32ce5431221f07
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-19 10:45:26 +0000

    Create Searchbar.vue

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { SearchIcon } from '@heroicons/vue/outline'
+import { ref } from 'vue';
+
+
+const search_term = ref('')
+
+const emit = defineEmits(['search', ])
+const props = defineProps(['search_term',])
+search_term.value = props.search_term || ''
+
+function search() {
+  emit('search', search_term.value)
+}
+
+</script>
+
+
+<template>
+  <form @submit.prevent="search" class="border-2 border-gray-300 rounded-lg p-4 flex items-center space-x-2 text-gray-400 shadow-md w-1/2">
+    <SearchIcon class="h-5 w-5" />
+    <input type="text" v-model="search_term" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black outline-none">
+    <button type="submit" class=""></button>
+  </form>
+  
+</template>
+
+
+<style lang="postcss" scoped>
+
+</style>
