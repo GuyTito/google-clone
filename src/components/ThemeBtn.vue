d091b6a17a8ac2f71e772a45ff9979191e0057b3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-20 11:01:13 +0000

    improved dark mode button

--- src/components/ThemeBtn.vue
+++ src/components/ThemeBtn.vue
@@ -8,11 +8,12 @@ const toggleDark = useToggle(isDark)
 
 
 <template>
-  <header>
-    <button @click="toggleDark()">
-      <span class="ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
-    </button>
-  </header>
+  <div>
+    <input @click="toggleDark()" id="toggle" type="checkbox" class="hidden"/>
+    <label for="toggle" class="w-9 h-5 flex items-center bg-gray-200 dark:bg-gray-500 rounded-full p-1 cursor-pointer duration-300 ease-in-out">
+      <div class="bg-white dark:bg-black w-4 h-4 rounded-full transform dark:translate-x-3 duration-300 ease-in-out"></div>
+    </label>
+  </div>
   
 </template>
 
