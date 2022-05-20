365996eb1e47d073d34b56f349f280e1e70b213e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-20 11:00:45 +0000

    dark mode

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -17,9 +17,9 @@ function search() {
 
 
 <template>
-  <form @submit.prevent="search" class="border-2 border-gray-300 rounded-lg p-2 flex items-center space-x-2 text-gray-400 shadow-md w-1/2">
+  <form @submit.prevent="search" class="border-2 border-gray-300 dark:border-gray-700 rounded-lg p-2 flex items-center space-x-2 text-gray-400 shadow-md w-full">
     <SearchIcon class="h-5 w-5" />
-    <input type="text" v-model="search_term" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black outline-none">
+    <input type="text" v-model="search_term" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black dark:text-gray-100 outline-none dark:bg-inherit">
     <button type="submit" class=""></button>
   </form>
   
