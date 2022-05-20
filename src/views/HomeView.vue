3e68380356a26d2b767a504dc5778866b6702336
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-20 11:01:25 +0000

    dark mode

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import Searchbar from '../components/Searchbar.vue';
+import ThemeBtn from '../components/ThemeBtn.vue';
 
 const router = useRouter()
 
@@ -13,7 +14,17 @@ function search(search_term) {
 </script>
 
 <template>
-  <div class="grid place-items-center min-h-screen">
-    <Searchbar @search="search"/>
+  <div >
+    <div class="mt-5 sm:mt-10 absolute right-5 sm:right-16">
+        <ThemeBtn />
+      </div>
+
+    <div class="absolute top-1/2 -translate-y-1/2 w-full">
+      <div class="mx-5 space-x-3 flex justify-between items-center sm:justify-center mt-5 sm:relative">
+        <div class="w-full sm:w-1/2">
+          <Searchbar @search="search"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
