2237db92c25937b7b85a1ceffc99805bb56aef65
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-05-23 11:40:51 +0000

    clear search and focus

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -13,17 +13,26 @@ function search() {
   emit('search', search_term.value)
 }
 
+const search_input = ref(null)
+function clearSearch() {
+  search_term.value = ''
+  search_input.value.focus()
+}
+
 </script>
 
 
 <template>
   <form @submit.prevent="search" class="border-2 border-gray-300 dark:border-gray-700 rounded-lg p-2 flex items-center space-x-2 text-gray-400 shadow-md w-full">
-    <SearchIcon class="h-5 w-5" />
-    <input type="text" v-model="search_term" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black dark:text-gray-100 outline-none dark:bg-inherit">
-    <button type="button" v-if="search_term" @click="search_term = ''">
+    
+    <input type="text" ref="search_input" v-model="search_term" placeholder="Search anything..." class="w-full placeholder-gray-400 text-black dark:text-gray-100 outline-none dark:bg-inherit">
+    <button type="button" v-if="search_term" @click="clearSearch" class="mr-2">
       <XIcon class="h-5 w-5"/>
     </button>
-    <button type="submit" class=""></button>
+    <div v-if="search_term" class="w-px h-6 bg-gray-400"></div>
+    <button type="submit">
+      <SearchIcon class="h-5 w-5 text-green-600" />
+    </button>
   </form>
   
 </template>
