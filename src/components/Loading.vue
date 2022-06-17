a9e673e036145bc04a1dc779e0b9e2fbf778a155
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-06-17 15:44:08 +0000

    Create Loading.vue

---
+++ src/components/Loading.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+  <div class="follow-the-leader">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  
+</template>
+
+
+<style lang="postcss" scoped>
+
+</style>
+
+<style scoped>
+@import '@/assets/whirl.css';
+
+</style>
