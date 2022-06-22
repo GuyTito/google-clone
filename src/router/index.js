e3861052778687b9ae585447b25aabcc3bee7619
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-06-22 14:09:24 +0000

    added images route

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,12 @@ const router = createRouter({
       // this generates a separate chunk (Search.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SearchView.vue')
-    }
+    },
+    {
+      path: '/images',
+      name: 'images',
+      component: () => import('../views/ImagesView.vue')
+    },
   ]
 })
 
