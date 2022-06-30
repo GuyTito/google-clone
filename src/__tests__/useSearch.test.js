dfc773109660a61f1e9f2e075131541d805e48e0
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-06-30 16:50:25 +0000

    Update useSearch.test.js

--- src/__tests__/useSearch.test.js
+++ src/__tests__/useSearch.test.js
@@ -1,7 +1,7 @@
 import { it, expect } from "vitest";
 import useSearch from "../composables/useSearch";
 
-it('returns promise which resolves to nothing if search text is not provided', () => {
+it('returns promise which resolves to nothing if search_text/page arg are not provided', () => {
   const res = useSearch('', '')
   expect(res).resolves.toBeUndefined()
 })
