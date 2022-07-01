856ccbcd6e635153991f2eb39e47409434ed3d3e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-07-01 14:10:22 +0000

    added footer and loading in middle
    loading animation in middle when view port smaller

--- src/views/BaseLayout.vue
+++ src/views/BaseLayout.vue
@@ -17,38 +17,48 @@ function search(search_text) {
 
 
 <template>
-  <div class="mx-2 space-x-3 flex justify-between items-center sm:justify-center mt-5 sm:relative">
-    <div class="sm:absolute sm:left-[10%] sm:top-1/2 sm:-translate-y-1/2">
-      <router-link to="/" class="font-bold text-xl text-green-600">goGGL</router-link>
+  <div class="flex flex-col min-h-screen">
+    <div class="mx-2 space-x-3 flex justify-between items-center sm:justify-center mt-5 sm:relative flex-shrink-0">
+      <div class="sm:absolute sm:left-[10%] sm:top-1/2 sm:-translate-y-1/2">
+        <router-link to="/" class="font-bold text-xl text-green-600">goGGL</router-link>
+      </div>
+      
+      <div class="w-full sm:w-1/2">
+        <Searchbar :search_term="search_term" @search="search"/>
+      </div>
+  
+      <div class="sm:absolute sm:right-[10%] sm:top-1/2 sm:-translate-y-1/2">
+        <ThemeBtn />
+      </div>
     </div>
+  
+    <div class="flex-grow">
+      <div class="mt-5 mx-5 sm:mx-14 md:max-w-3xl md:mx-auto">
+        <nav class="space-x-5 sm:space-x-10 flex sm:block justify-between mb-5 sm:mb-8 border-b-2 dark:border-gray-700 text-gray-400">
+          <router-link 
+            v-for="item in menu" :key="item" 
+            :to="`/${item.toLowerCase()}?q=${search_term}`" 
+            class="border-b-4 border-transparent py-2 px-4 inline-block "
+          >
+            {{ item == 'search' ? 'All' : item }}
+          </router-link>
+        </nav>
     
-    <div class="w-full sm:w-1/2">
-      <Searchbar :search_term="search_term" @search="search"/>
+        <div v-if="load" class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">
+          <Loading />
+        </div>
+        <div v-else>
+          <slot></slot>
+        </div>
+      </div>
     </div>
-
-    <div class="sm:absolute sm:right-[10%] sm:top-1/2 sm:-translate-y-1/2">
-      <ThemeBtn />
-    </div>
+  
+    <footer class="mt-5 pt-5 pb-5 text-center text-[10px] text-gray-600/80 bg-gray-200 flex-shrink-0">
+      Inspired by <a href="https://youtu.be/GDa8kZLNhJ4?t=28148" target="_blank" class="underline hover:font-bold">JavaScript Mastery</a>. 
+         <a href="https://github.com/GuyTito/google-clone"  target="_blank" class="underline hover:font-bold">Source Code</a>.
+    </footer>
   </div>
 
-  <div class="mt-5 mx-5 sm:mx-14 md:max-w-3xl md:mx-auto">
-    <nav class="space-x-5 sm:space-x-10 flex sm:block justify-between mb-5 sm:mb-8 border-b-2 dark:border-gray-700 text-gray-400">
-      <router-link 
-        v-for="item in menu" :key="item" 
-        :to="`/${item.toLowerCase()}?q=${search_term}`" 
-        class="border-b-4 border-transparent py-2 px-4 inline-block "
-      >
-        {{ item == 'search' ? 'All' : item }}
-      </router-link>
-    </nav>
-
-    <div v-if="load" class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ">
-      <Loading />
-    </div>
-    <div v-else>
-      <slot></slot>
-    </div>
-  </div>
 </template>
 
 
